dff465db6aa553fc7a69239fe5dd3bdd3802b2dc
ThoPoi <thomas.poirier@hetic.net>
2019-04-08 15:37:49 +0200

    add advancedFilter.php

---
+++ controllers/advancedFilters.php
@@ -0,0 +1,5 @@
+<?php
+
+$title = 'Advanced Filters';
+
+include '../views/pages/advancedFilters.php';
--- views/pages/about.php
+++ views/pages/about.php
@@ -1,5 +1,5 @@
 <?php include '../views/partials/header.php' ?>
 
-<h1>About</h1>
+<h1>Advanced Filters</h1>
 
 <?php include '../views/partials/footer.php' ?>
