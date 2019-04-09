e04364fa7bb60d4293497b7f23def6b2394f3a10
ThoPoi <thomas.poirier@hetic.net>
2019-04-09 16:43:12 +0200

    add main search

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,1 +1,9 @@
-console.log('All good')
+const mainSearch = document.querySelector('.main_search')
+const searchFilterAll = mainSearch.querySelectorAll('.search_filter')
+const searchSelect = mainSearch.querySelector('.search_select')
+
+searchFilterAll.forEach(function(searchFilter) {
+    searchFilter.addEventListener('click', () => {
+        searchSelect.classList.toggle('search_filter-block')
+    })
+})
