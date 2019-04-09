118ef6b1a53b7ee19f5298ade51da796addba75e
Kérian Pelat <narty.dev@gmail.com>
2019-04-09 22:55:12 +0200

    :sparkless: add map & events

--- controllers/map.php
+++ controllers/map.php
@@ -2,4 +2,24 @@
 
 $title = 'Map';
 
+$url = 'https://app.ticketmaster.com/discovery/v2/events.json?';
+$url .= http_build_query([
+    'apikey' => '3gDxxU8POu1umcTRMNSF4AGqgtUzc2md',
+    'size' => '200' 
+]);
+
+// Make request to API
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+$result = curl_exec($curl);
+curl_close($curl);
+$result = json_decode($result);
+// Create static map URL
+
+
 include '../views/pages/map.php'; ?>
--- public/assets/script.js
+++ public/assets/script.js
@@ -1,4 +1,4 @@
-const mainSearch = document.querySelector('.main_search')
+/*const mainSearch = document.querySelector('.main_search')
 const searchFilterAll = mainSearch.querySelectorAll('.search_filter')
 const searchSelect = mainSearch.querySelector('.search_select')
 
@@ -6,4 +6,4 @@ searchFilterAll.forEach(function(searchFilter) {
     searchFilter.addEventListener('click', () => {
         searchSelect.classList.toggle('search_filter-block')
     })
-})
+})*/
